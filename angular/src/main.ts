d22dd180fe045e84bfbeb1383fb6433e724cda9e
FERHATSUMER <sumerf39>
2023-06-19 15:34:29 +0300

    angular-app

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -6,4 +6,6 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
+// Path: angular\src\app\app.module.ts// Path: angular\src\app\app.module.ts
 
+//  import { BrowserModule } from '@angular/platform-browser';
